f8b7962f97c2685f41f5d2b0767ed30d0205d498
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-09-30 03:38:41 +0700

    Create stres.js

---
+++ plugins/stres.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+let handler = async (m, { conn }) => {
+  let caption = `
+*NGACA WOY* 🖕😅 
+`.trim()
+  m.reply(caption)
+}
+handler.help = ['stres']
+handler.tags = ['other']
+handler.command = /^(stres)$/i
+handler.limit = true
+
+export default handler
