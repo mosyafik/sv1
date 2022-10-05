da3a94da1c41f478a162ee3b706d6baa0509a776
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-10-05 23:19:19 +0700

    Update fun-how.js

--- plugins/fun-how.js
+++ plugins/fun-how.js
@@ -1,14 +1,15 @@
 let handler = async (m, { conn, command, text, usedPrefix }) => {
     if (!text) throw `Use example ${usedPrefix}${command} i'm`
     conn.reply(m.chat, `
-  ${command} *${text}*
-  *${text}* is *${(101).getRandom()}*% ${command.replace('how', '').toUpperCase()}
+  Hasilnya adalah :
+
+  *${text}* *${(101).getRandom()}*% ${command.replace('cek', '').toUpperCase()} coy
   `.trim(), m, m.mentionedJid ? {
         mentions: m.mentionedJid
     } : {})
 }
 handler.help = ['gay', 'pintar', 'cantik', 'ganteng', 'gabut', 'gila', 'lesbi', 'stress', 'bucin', 'jones', 'sadboy'].map(v => 'how' + v + ' siapa?')
 handler.tags = ['kerang', 'fun']
-handler.command = /^how(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
+handler.command = /^cek(gay|pintar|cantik|ganteng|gabut|gila|lesbi|stress?|bucin|jones|sadboy)/i
 
 export default handler
