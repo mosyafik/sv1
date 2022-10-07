b28587f67623208b3602815f87135f54d8bca6e0
Mosyafik Jr <63044745+mosyafik@users.noreply.github.com>
2022-10-07 23:45:32 +0700

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,3 +1,44 @@
+// --------------------------------------------- pakai scrape social_media_downloader --------------------------------------------------------
+/*
+import fetch from 'node-fetch'
+import axios from 'axios'
+import { tiktok } from "social_media_downloader"
+let handler = async (m, { conn, usedPrefix, command, text, args }) => {
+if (!args[0]) throw 'Masukkan Link'
+try {
+let p = await tiktok(args[0])
+    if (!p.link) throw 'Can\'t download video!'
+    let cap = `*「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
+                 ████████▀▀▀████
+                 ████████────▀██
+                 ████████──█▄──█
+                 ███▀▀▀██──█████
+                 █▀──▄▄██──█████
+                 █──█████──█████
+                 █▄──▀▀▀──▄█████
+                 ███▄▄▄▄▄███████
+────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
+*Nickname:* ${p.dev}
+*Description:* ${p.description}
+*Url:* ${p.url}
+
+_©BotShin🍭_
+`.trim()
+conn.send2ButtonVid(m.chat, p.link, cap, author, `Donasi`, `.donasi`, `Audio`, `.tta ${args[0]}`, fakes, adReply)
+} catch (e) {
+    throw eror
+    }
+}
+
+handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
+
+export default handler
+
+// --------------------------------------------- pakai scrape social_media_downloader --------------------------------------------------------
+/*
+
 import fetch from 'node-fetch'
 import axios from 'axios'
 import { tiktokdl, tiktokdlv2, tiktokdlv3 } from '@bochilteam/scraper'
@@ -37,44 +78,3 @@ handler.tags = ['downloader']
 handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
 
 export default handler
-
-// --------------------------------------------- pakai scrape social_media_downloader --------------------------------------------------------
-/*
-import fetch from 'node-fetch'
-import axios from 'axios'
-import { tiktok } from "social_media_downloader"
-let handler = async (m, { conn, usedPrefix, command, text, args }) => {
-if (!args[0]) throw 'Masukkan Link'
-try {
-let p = await tiktok(args[0])
-    if (!p.link) throw 'Can\'t download video!'
-    let cap = `*「 🇹 ᴛ ɪ ᴋ ᴛ ᴏ ᴋ 」*
-                 ████████▀▀▀████
-                 ████████────▀██
-                 ████████──█▄──█
-                 ███▀▀▀██──█████
-                 █▀──▄▄██──█████
-                 █──█████──█████
-                 █▄──▀▀▀──▄█████
-                 ███▄▄▄▄▄███████
-────────── ⇆ㅤ◁ㅤ ❚❚ㅤ ▷ㅤ↻ ──────────
-*Nickname:* ${p.dev}
-*Description:* ${p.description}
-*Url:* ${p.url}
-
-_©BotShin🍭_
-`.trim()
-conn.send2ButtonVid(m.chat, p.link, cap, author, `Donasi`, `.donasi`, `Audio`, `.tta ${args[0]}`, fakes, adReply)
-} catch (e) {
-    throw eror
-    }
-}
-
-handler.help = ['tiktok', 'tiktok', 'tiktokdl'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-handler.command = /^(tik(tok)?(tok)?(dl)?)$/i
-
-export default handler
-/*
-// --------------------------------------------- pakai scrape social_media_downloader --------------------------------------------------------
-/*
